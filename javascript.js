e5490c5b2c6c3cef21ff43993a88786cd2d7d853
larissa lotin <131203128+lotinn@users.noreply.github.com>
2023-12-10 16:42:14 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,32 +1,37 @@
 let botaoadiconar = document.getElementById("adicionar");
-let containertarefa = document.getElementById("containertarefa");
-let campoinput = document.getElementById("camporesultado")
-let botaoterminar = document.getElementById("terminar");
-let botaoapagar = document.getElementById("apagar");
-let tarefacompleta = document.getElementById("containertarefacompleta")
+        let containertarefa = document.getElementById("containertarefa");
+        let campoinput = document.getElementById("camporesultado");
+        let botaoterminar = document.getElementById("terminar");
+        let botaoapagar = document.getElementById("apagar");
+        let tarefacompleta = document.getElementById("containertarefacompleta");
 
+        botaoadiconar.addEventListener("click", function () {
+            let paragrafo = document.createElement("p");
+            paragrafo.classList.add('paragrafoestilo');
+            paragrafo.innerText = campoinput.value;
+            containertarefa.appendChild(paragrafo);
+            campoinput.value = "";
+            campoinput.focus();
 
-botaoadiconar.addEventListener("click", function () {
-    let paragrafo = document.createElement("p");
-    paragrafo.classList.add('paragrafoestilo');       
-    paragrafo.innerText = campoinput.value;
-    containertarefa.appendChild(paragrafo);
-    campoinput.value = "";
-    campoinput.focus()
+            paragrafo.addEventListener("click", function () {
+                containertarefa.removeChild(paragrafo);
+                tarefacompleta.appendChild(paragrafo);   //adicionar paragrafo quando um item vai para a lista
+                campoinput.focus();
+            });
+        });
 
-    paragrafo.addEventListener("click", function () {
-        containertarefa.removeChild(paragrafo);
-        tarefacompleta.appendChild(paragrafo);   //adiconar paragrafo quando um item vai pra lista
-        campoinput.focus();
-    })
-    botaoterminar.addEventListener("click", function () {
-        containertarefa.removeChild(paragrafo);     //terminar todas as tarefas
-        tarefacompleta.appendChild(paragrafo);
-        campoinput.focus();
-    })
-    botaoapagar.addEventListener("click", function () {
-        tarefacompleta.removeChild(paragrafo);  //apagar as tarefas prontas
-        campoinput.focus();
-    })
+        botaoterminar.addEventListener("click", function () {
+            while (containertarefa.firstChild) {
+                let paragrafo = containertarefa.firstChild;
+                containertarefa.removeChild(paragrafo);
+                tarefacompleta.appendChild(paragrafo);
+            }
+            campoinput.focus();
+        });
 
-})
+        botaoapagar.addEventListener("click", function () {
+            while (tarefacompleta.firstChild) {
+                tarefacompleta.removeChild(tarefacompleta.firstChild);
+            }
+            campoinput.focus();
+        });
