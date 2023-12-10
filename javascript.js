d757430d680ce01207c0e8dfb663cb8700c90712
larissa lotin <131203128+lotinn@users.noreply.github.com>
2023-12-10 17:20:21 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,37 +1,82 @@
+// Seleção de elementos do DOM
 let botaoadiconar = document.getElementById("adicionar");
-        let containertarefa = document.getElementById("containertarefa");
-        let campoinput = document.getElementById("camporesultado");
-        let botaoterminar = document.getElementById("terminar");
-        let botaoapagar = document.getElementById("apagar");
-        let tarefacompleta = document.getElementById("containertarefacompleta");
+let containertarefa = document.getElementById("containertarefa");
+let campoinput = document.getElementById("camporesultado");
+let botaoterminar = document.getElementById("terminar");
+let botaoapagar = document.getElementById("apagar");
+let tarefacompleta = document.getElementById("containertarefacompleta");
 
-        botaoadiconar.addEventListener("click", function () {
-            let paragrafo = document.createElement("p");
-            paragrafo.classList.add('paragrafoestilo');
-            paragrafo.innerText = campoinput.value;
-            containertarefa.appendChild(paragrafo);
-            campoinput.value = "";
-            campoinput.focus();
+// Função para adicionar uma nova tarefa
+function adicionarTarefa(paragrafo) {
+    // Adiciona a classe de estilo ao parágrafo
+    paragrafo.classList.add('paragrafoestilo');
+    // Adiciona o parágrafo ao container de tarefas
+    containertarefa.appendChild(paragrafo);
 
-            paragrafo.addEventListener("click", function () {
-                containertarefa.removeChild(paragrafo);
-                tarefacompleta.appendChild(paragrafo);   
-                campoinput.focus();
-            });
-        });
+    // Cria um botão "Pronta" ao invés de "Terminar"
+    let botaoPronta = document.createElement("button");
+    botaoPronta.innerText = "Pronta";
+    botaoPronta.addEventListener("click", function () {
+        // Move o parágrafo para o container de tarefas concluídas
+        containertarefa.removeChild(paragrafo);
+        tarefacompleta.appendChild(paragrafo);
+        // Esconde o botão "Pronta"
+        botaoPronta.style.display = "none";
+        // Coloca o foco no campo de entrada
+        campoinput.focus();
+    });
 
-        botaoterminar.addEventListener("click", function () {
-            while (containertarefa.firstChild) {
-                let paragrafo = containertarefa.firstChild;
-                containertarefa.removeChild(paragrafo);
-                tarefacompleta.appendChild(paragrafo);
-            }
-            campoinput.focus();
-        });
+    // Adiciona o botão "Pronta" ao lado do parágrafo
+    containertarefa.appendChild(paragrafo);
+    containertarefa.appendChild(botaoPronta);
 
-        botaoapagar.addEventListener("click", function () {
-            while (tarefacompleta.firstChild) {
-                tarefacompleta.removeChild(tarefacompleta.firstChild);
-            }
-            campoinput.focus();
-        });
+    // Limpa o campo de entrada e coloca o foco novamente
+    campoinput.value = "";
+    campoinput.focus();
+}
+
+// Adiciona uma nova tarefa quando a tecla Enter é pressionada no campo de entrada
+campoinput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        // Cria um novo parágrafo
+        let paragrafo = document.createElement("p");
+        paragrafo.innerText = campoinput.value;
+        // Chama a função para adicionar a tarefa
+        adicionarTarefa(paragrafo);
+    }
+});
+
+// Adiciona uma nova tarefa quando o botão "Adicionar" é clicado
+botaoadiconar.addEventListener("click", function () {
+    // Cria um novo parágrafo
+    let paragrafo = document.createElement("p");
+    paragrafo.innerText = campoinput.value;
+    // Chama a função para adicionar a tarefa
+    adicionarTarefa(paragrafo);
+});
+
+// Move todas as tarefas para o container de tarefas concluídas
+botaoterminar.addEventListener("click", function () {
+    // Obtém todos os parágrafos dentro do container de tarefas
+    let paragrafos = Array.from(containertarefa.getElementsByTagName("p"));
+    // Para cada parágrafo, move para o container de tarefas concluídas e esconde o botão "Pronta"
+    paragrafos.forEach(paragrafo => {
+        let botaoPronta = paragrafo.nextElementSibling;
+        containertarefa.removeChild(paragrafo);
+        tarefacompleta.appendChild(paragrafo);
+        if (botaoPronta) {
+            botaoPronta.style.display = "none";
+        }
+    });
+    // Coloca o foco no campo de entrada
+    campoinput.focus();
+});
+
+// Apaga todas as tarefas do container de tarefas concluídas
+botaoapagar.addEventListener("click", function () {
+    while (tarefacompleta.firstChild) {
+        tarefacompleta.removeChild(tarefacompleta.firstChild);
+    }
+    // Coloca o foco no campo de entrada
+    campoinput.focus();
+});
